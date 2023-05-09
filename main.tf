61e14e65f86f093b87c0264e324d3b16a028d5cf
sbcp071 <hamichhantyal@hotmail.com>
2023-05-08 22:49:42 -0400

    module for SGs

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ module "blog_sg" {
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules = ["http-80-tcp", "https-443-tcp"]
-  engress_cidr_blocks = ["0.0.0.0/0"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
