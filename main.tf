a4a9ce128839acf8a066c6fe4db6aa34028fffc2
sbcp071 <hamichhantyal@hotmail.com>
2023-05-09 00:25:19 -0400

    Creating VPC

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
@@ -45,13 +45,12 @@ vpc_security_group_ids = [module.blog_sg.security_group_id]
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.2"
-  name = "blog_new"
 
+  name = "blog
   vpc_id = module.vpc.public_subnets[0]
   
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules = ["http-80-tcp", "https-443-tcp"]
+  egress_rules = ["all", "all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
