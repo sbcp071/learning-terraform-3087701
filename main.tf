501cb701d4226468364b2da20c2e98700d06afee
sbcp071 <hamichhantyal@hotmail.com>
2023-05-09 02:55:44 -0400

    c

--- main.tf
+++ main.tf
@@ -46,10 +46,10 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.blog_vpc.vpc_id
-  name    = "blog"
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  name                = "blog"
+  vpc_id              = module.blog_vpc.vpc_id
+  ingress_rules       = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
