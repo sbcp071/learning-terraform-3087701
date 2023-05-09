4e313837de70a665ecdea2382aa2e28abb5f39b8
sbcp071 <hamichhantyal@hotmail.com>
2023-05-09 01:14:57 -0400

    cr1

--- main.tf
+++ main.tf
@@ -32,10 +32,10 @@ module "vpc" {
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-vpc_security_group_ids = [module.blog_sg.security_group_id]
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  subet_id               = module.blog_vpc.public_subnets[0]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
@@ -46,9 +46,8 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.2"
 
+  vpc_id = module.blog_vpc.vpc_id
   name = "blog"
-  vpc_id = module.vpc.public_subnets[0]
-  
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all", "all"]
